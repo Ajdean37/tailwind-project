3e609a7a4d4fca22cd79033af366ea0d4f02202b
Ajdean37 <ajdean37@gmail.com>
2022-05-20 12:35:59 -0500

    changes

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="chat-notification">
+      <div className="chat-notification-logo-wrapper">
+    </div>
+    <div className="chat-notification-content">
+      <h4 className="chat-notification-title">ChitChat</h4>
+      <p className="chat-notification-message">You have a new message!</p>
+    </div>
+</div>
+  );
+}
+
+export default App;
